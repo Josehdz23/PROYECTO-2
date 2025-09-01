a47abd3e5ca7066f84e9b404591738b0b1535531
Josehdz23 <jose.jahr.2016@gmail.com>
2025-09-01 17:30:12 -0600

    Ya solo me falta agregar el nit del proveedor al detalle compra, ya que el codigo de empleado lo tengo así que tengo toda la info, por ende solo me quedaría agregar como otro detalle de compra el nit del proveedor que tiene todos los datos de a quién se le compraría

--- categorias.py
+++ categorias.py
@@ -1,3 +1,5 @@
+from productos import Producto
+
 categorias = {}
 class Categoria:
     def __init__(self, nombre):
@@ -9,13 +11,19 @@ class Categoria:
 
 class RegistroCategoria:
     def agregarCategoria(self,id,producto,cantidad):
+        b = 0
         if id in categorias:
             for obj in categorias[id]["productos"]:
                 if obj.nombre == producto.nombre:
                     obj.stock += cantidad
+                    b = 1
                     return categorias[id]["nombre"].nombre
+            if b == 0:
+                categorias[id]["productos"].append(producto)
+                return categorias[id]["nombre"].nombre
         else:
             nombre = input("La categoria no existía pero se creará, ingrese el nombre del categoría: ")
+            producto.categoria = nombre
             categorias[id] = {
                 "nombre": Categoria(nombre),
                 "productos": [producto]
@@ -23,7 +31,6 @@ class RegistroCategoria:
             return nombre
     def mostrarCategorias(self):
         for clave, datos in categorias.items():
-            print(f"IDCategoria: {clave}")
+            print(f"IDCategoria: {clave} | Categoria: {datos['nombre'].nombre}")
             for datos2 in datos["productos"]:
                 print(f"- {datos2}")
-
